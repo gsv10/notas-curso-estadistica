66967005fa60140533f772e2c2c381265dab2b18
Maikol Solís <maikol.solis@ucr.ac.cr>
2020-05-06 10:58:12 -0600

    Agrega archivos de configuración

---
+++ _common.R
@@ -0,0 +1,15 @@
+rm(list = ls())
+
+options(tinytex.verbose = TRUE)
+
+options(knitr.duplicate.label = "allow")
+knitr::opts_chunk$set(
+  cache = TRUE,
+  tidy = TRUE,
+  tidy.opts = list(width.cutoff = 50),
+  error = TRUE, 
+  warning = FALSE
+)
+
+library(tidyverse)
+
